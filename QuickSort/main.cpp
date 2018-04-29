2837207541a3484da75509fa74beb1bc80865f3b
zhiyong <zhiyong_ncu@163.com>
2018-04-29 20:04:39 +0800

    快速排序

---
+++ QuickSort/main.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+#include"SortTestHelper.h"
+
+using namespace std;
+
+// 对arr[l...r]部分进行partition操作
+// 返回p, 使得arr[l...p-1] < arr[p] ; arr[p+1...r] > arr[p]
+template<typename T>
+int __partition(T arr[], int left, int rigth) {
+	
+	T e = arr[left];
+	int j = left;
+	for (int i=left+1; i<=rigth; ++i) {   //使得 arr[l+1...j] < v ; arr[j+1...i) > v
+		if (arr[i] < e) {
+			j++;
+			swap(arr[i], arr[j]);
+		}
+	}
+	
+	//交换left和j的元素 
+	swap(arr[left], arr[j]);
+	
+	return j;
+	
+}
+
+// 对arr[l...r]部分进行快速排序
+template<typename T>
+void __quickSort(T arr[], int left, int right) {
+	
+	if (left >= right) {
+		return;
+	}
+	
+	int p = __partition(arr, left, right);
+	__quickSort(arr, left, p);
+	__quickSort(arr, p+1, right);
+}
+
+
+template<typename T>
+void quickSort(T arr[],int n) {
+	
+	__quickSort(arr,0,n-1);
+	
+	return;
+}
+
+int main() {
+	int n = 100;
+	int * arr = SortTestHelper::generateRandomArray(n,0,n);
+	SortTestHelper::printArray(arr,n);
+	quickSort(arr,n);
+	SortTestHelper::printArray(arr,n);
+	//SortTestHelper::testSort("insertSort",insertionSort,arr2,n);
+
+	
+	delete[] arr;
+		
+	return 0;
+}
